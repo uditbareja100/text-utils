184a88f6676c4099353d1ddbb1acfd1d57838719
uditbareja100 <uditbareja1998@gmail.com>
2022-11-10 14:39:14 +0530

    About page updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
     <>
-     <h1>About the TextUtils</h1> 
+      <div className="container">
+        <h1>About the TextUtils</h1>
+        <div className="container">
+          <p>
+            We provide free tools to help you with your daily tasks. You will
+            find tools for formatting source code, converters, tools for
+            handling text, such as remove duplicate characters, empty lines,
+            text sorter and many others.
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
